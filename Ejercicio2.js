ca3d44c07bb801c85b3b33f07405905ff2b363be
niscely <romeronisely@gmail.com>
2022-11-15 20:12:21 -0500

    se envia el archivo

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -5,8 +5,8 @@ let abancay = monto * 0.25
 let julio = monto * 0.10
 let aviacion = monto * 0.15
 let tacna = monto * 0.15
-alert("Avenida La Mar:" + ar )
+alert("Avenida La Mar:" + mar )
 alert("Avenida Abancay:" + abancay )
-alert("Avenida 28 de Julio:" + julio )
+alert("Avenida 28 de julio:" + julio )
 alert("Avenida Aviacion:" + aviacion )
-alert("Avenida Tacma:" + tacna )
+alert("Avenida Tacna:" + tacna )
